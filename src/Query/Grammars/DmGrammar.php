36e85df0ef15beb5625b9bdaed850f649d7771b4
lxrmido <lxrmido@qq.com>
2023-07-04 17:17:10 +0800

    getDateFormat

--- src/Query/Grammars/DmGrammar.php
+++ src/Query/Grammars/DmGrammar.php
@@ -590,4 +590,14 @@ class DmGrammar extends Grammar
 
         return $sql.' from ('.$this->compileSelect($query).') '.$this->wrapTable('temp_table');
     }
+
+    /**
+     * Get the format for database stored dates.
+     *
+     * @return string
+     */
+    public function getDateFormat()
+    {
+        return 'Y-m-d H:i:s.000';
+    }
 }
